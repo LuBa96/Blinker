950d3a9d68121886e99573a1cb34279c1a9609aa
LuBa96 <lukas.bachmann96@gmx.net>
2017-01-14 20:52:47 +0100

    Add files via upload

---
+++ Blinkschaltung/Debug/subdir.mk
@@ -0,0 +1,37 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../rpi.c 
+
+S_UPPER_SRCS += \
+../gpio_functions.S \
+../main.S 
+
+OBJS += \
+./gpio_functions.o \
+./main.o \
+./rpi.o 
+
+C_DEPS += \
+./rpi.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.S
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross GCC Assembler'
+	arm-linux-gnueabihf-as -g -gstabs  -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+%.o: ../%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross GCC Compiler'
+	arm-linux-gnueabihf-gcc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
